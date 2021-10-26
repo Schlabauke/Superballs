9e8363d4b9765c727434c7202d8686a92e54283b
Schlabauke <annabelle.jeffre@gmail.com>
2021-10-26 15:11:19 +0200

    Superballs

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+function kreis1() {
+    body.style.background = "rgb(2, 133, 2)";
+    h1.style.color = "purple";
+}
+
+
+function kreis2() {
+    body.style.background = "rgb(88, 37, 182)";
+    h1.style.color = "orange";
+}
+
+
+function kreis3() {
+    body.style.background = "black";
+    h1.style.color = "rgb(247, 100, 3)";
+}
+
+function kreis4() {
+    body.style.background = "rgb(1, 1, 73)";
+    h1.style.color = "rgb(0, 47, 255)";
+}
